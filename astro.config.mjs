6cdae030a8b06d7240aca1f8ec2655efac76b759
Walther Seidel <waseidev@gmail.com>
2024-08-09 00:30:57 +0200

    Deleting dependencie astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://waseidev.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
-
