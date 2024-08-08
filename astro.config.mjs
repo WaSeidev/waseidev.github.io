28dab2c729743c7074a55ca57e50f4bf9c1eb9cc
Walther Seidel <waseidev@gmail.com>
2024-08-09 00:10:18 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://waseidev.github.io",
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+});
+
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Portafolio | WaSeidev";
+export const SITE_DESCRIPTION = "Portafolio de Walther Seidel!";
