e7047c6b01b31c5e62d702149dc97f0c9458458e
Walther Seidel <waseidev@gmail.com>
2024-08-10 15:33:14 +0200

    fixing layout and icons

---
+++ src/cv.d.ts
@@ -0,0 +1,103 @@
+export interface CV {
+  basics: Basics;
+  work: Work[];
+  volunteer: Volunteer[];
+  education: Education[];
+  certificates: Certificate[];
+  skills: Skill[];
+  languages: Language[];
+  interests: Interest[];
+  references: Reference[];
+  projects: Project[];
+}
+
+export interface Basics {
+  name: string;
+  label: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  summary: string;
+  location: Location;
+  profiles: Profile[];
+}
+
+export interface Location {
+  address: string;
+  postalCode: string;
+  city: string;
+  countryCode: string;
+  region: string;
+}
+
+export interface Profile {
+  network: string;
+  username: string;
+  url: string;
+}
+
+export interface Work {
+  name: string;
+  position: string;
+  url: string;
+  startDate: string;
+  endDate: string;
+  summary: string;
+  highlights: string[];
+}
+
+export interface Volunteer {
+  organization: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  summary: string;
+  highlights: string[];
+}
+
+export interface Education {
+  institution: string;
+  url: string;
+  area: string;
+  studyType: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface Certificate {
+  name: string;
+  url: string;
+  issuer: string;
+  date: string;
+}
+
+export interface Skill {
+  name: string;
+  level: string;
+  keywords: string[];
+}
+
+export interface Language {
+  language: string;
+  fluency: string;
+}
+
+export interface Interest {
+  name: string;
+  keywords: string[];
+}
+
+export interface Reference {
+  name: string;
+  reference: string;
+}
+
+export interface Project {
+  name: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  highlights: string[];
+  url: string;
+}
